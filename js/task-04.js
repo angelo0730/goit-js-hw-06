8856aad2e9131bc677453b463a621007ebc23215
angelo0730 <137179844+angelo0730@users.noreply.github.com>
2023-11-21 21:36:55 +0800

    update

--- js/task-04.js
+++ js/task-04.js
@@ -1,29 +1,60 @@
+// class Counter {
+//   constructor(selector, step, initialValue) {
+//     this.counterContainer = document.querySelector(selector);
+//     this.incrementBtn = this.counterContainer.querySelector("[data-action='increment']");
+//     this.dicrementBtn = this.counterContainer.querySelector("[data-action='decrement']");
+//     this.spanRef = this.counterContainer.querySelector('#value');
+//     this.step = step;
+//     this.initialValue = initialValue;
+//     this.addListeners();
+//   }
+//   addListeners = () => {
+//     this.spanRef.textContent = this.initialValue;
+//     this.incrementBtn.textContent = `+ ${this.step}`;
+//     this.dicrementBtn.textContent = `- ${this.step}`;
+//     this.incrementBtn.addEventListener('click', this.hendelIncriment);
+//     this.dicrementBtn.addEventListener('click', this.hendelDecrement);
+//   };
+//   hendelIncriment = () => {
+//     this.initialValue += this.step;
+//     this.spanRef.textContent = this.initialValue;
+//   };
+//   hendelDecrement = () => {
+//     this.initialValue -= this.step;
+//     this.spanRef.textContent = this.initialValue;
+//   };
+// }
+// const counter1 = new Counter('#counter', 5, 0);
+// // counter1.addListeners();
+// const counter2 = new Counter('#counter2', 10, 100);
+
+
 class Counter {
-  constructor(selector, step, initialValue) {
+  constructor(selector, initialValue) {
     this.counterContainer = document.querySelector(selector);
-    this.incrementBtn = this.counterContainer.querySelector("[data+action='increment']");
-    this.dicrementBtn = this.counterContainer.querySelector("[data-action='decrement']");
+    this.incrementBtn = this.counterContainer.querySelector("[data-action='increment']");
+    this.decrementBtn = this.counterContainer.querySelector("[data-action='decrement']");
     this.spanRef = this.counterContainer.querySelector('#value');
-    this.step = step;
     this.initialValue = initialValue;
     this.addListeners();
   }
+
   addListeners = () => {
     this.spanRef.textContent = this.initialValue;
-    this.incrementBtn.textContent = `+ ${this.step}`;
-    this.dicrementBtn.textContent = `- ${this.step}`;
-    this.incrementBtn.addEventListener('click', this.hendelIncriment);
-    this.dicrementBtn.addEventListener('click', this.hendelDecrement);
+    this.incrementBtn.addEventListener('click', this.handleIncrement);
+    this.decrementBtn.addEventListener('click', this.handleDecrement);
   };
-  hendelIncriment = () => {
-    this.initialValue += this.step;
+
+  handleIncrement = () => {
+    this.initialValue++;
     this.spanRef.textContent = this.initialValue;
   };
-  hendelDecrement = () => {
-    this.initialValue -= this.step;
+
+  handleDecrement = () => {
+    this.initialValue--;
     this.spanRef.textContent = this.initialValue;
   };
 }
-const counter1 = new Counter('#counter', 5, 0);
-// counter1.addListeners();
-const counter2 = new Counter('#counter2', 10, 100);
+
+const counter1 = new Counter('#counter', 0);
+const counter2 = new Counter('#counter2', 100);
